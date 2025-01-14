ca9a7254da9559ff2668a933d51fd83dcc8a41c8
Joey <9813078+PXshadow@users.noreply.github.com>
2025-01-14 10:02:15 +0100

    Graph: onClick jump to commit, tooltip display pass/total

--- test883/Graph.hx
+++ test883/Graph.hx
@@ -1,3 +1,4 @@
+import js.html.CanvasElement;
 import js.html.SelectElement;
 #if js
 import haxe.Json;
@@ -52,6 +53,7 @@ function run(test:String, target:String) {
 			y: (result.passing / result.total) * 100,
 			total: result.total,
             commit: result.commit,
+            passes: result.passing,
 		});
 	}
 	var data = {
@@ -72,12 +74,14 @@ function run(test:String, target:String) {
         },
         plugins: {
 			tooltip: {
+                //enabled: false,
 				callbacks: {
 					title: item -> (item[0].raw.x : js.lib.Date).toUTCString(),
-					footer: item -> "passing: " + [
+					/*footer: item -> "passing: " + [
 						for (obj in (item : Array<Dynamic>))
 							Math.round((obj.parsed.y * obj.raw.total) / 100)
-					],
+					],*/
+                    label: item -> item.dataset.label + ":" + item.raw.passes + "/" + item.raw.total,
 				}
 			},
 			legend: {
@@ -104,6 +108,7 @@ function run(test:String, target:String) {
         },
     }
 	};
+    var ctx:CanvasElement = null;
 	Syntax.code("
         var ctx = document.getElementById('chart');
         var chart = new Chart(ctx,{
@@ -116,7 +121,18 @@ function run(test:String, target:String) {
             options: {1},
         });
         {2} = chart;
-    ", data, options, chart);
+        {3} = ctx;
+    ", data, options, chart, ctx);
+    //Console.log(chart);
+    ctx.onclick = e -> {
+        final points = chart.getElementsAtEventForMode(e, 'nearest', { intersect: true }, true);
+        if (points.length == 0)
+            return;
+        final firstPoint = points[0];
+        final label = chart.data.labels[firstPoint.index];
+        final value = chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
+        Browser.window.open("https://github.com/go2hx/go2hx/commit/" + value.commit, "_blank");
+    }
 }
 
 var chart = null;
