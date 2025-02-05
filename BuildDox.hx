a045930f4734cb4a015957375b02737b11d6ae92
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 22:19:02 +0100

    dox: regexp2 build here

--- BuildDox.hx
+++ BuildDox.hx
@@ -1,17 +1,29 @@
 function main() {
-    if (!sys.FileSystem.exists("page/api"))
-        sys.FileSystem.createDirectory("page/api");
-    
-    if (sys.FileSystem.exists("page/api/hashes/go2hx.md5"))
-        sys.FileSystem.deleteFile('page/api/hashes/go2hx.md5');
-    sys.io.File.copy("page/logoapi.svg", "page/api/logo.svg");
-    addImports();
-    Sys.command("haxe dox.hxml");
-    final version = haxe.Json.parse(sys.io.File.getContent("go2hx/haxelib.json")).version;
-    Sys.command('haxelib run dox -D description "Go to Haxe source-to-source compiler" -D toplevel-package stdgo -D website https://go2hx.github.io -D logo logo.svg -i go2hx.xml -o page/api --title "go2hx Compiler" -D version $version -in stdgo -D source-path "https://github.com/go2hx/go2hx/blob/master/"');
+
+    Sys.command("rm -d -r page/api/hashes");
+    //if (sys.FileSystem.exists("page/api/hashes/go2hx.md5"))
+    //    sys.FileSystem.deleteFile('page/api/hashes/go2hx.md5');
+    // std
+    addStdImports();
+    createDox("go2hx Compiler", "Go to Haxe source-to-source compiler", "go2hx", "stdgo", "stdgo", "api", "-cp go2hx");
+    // regexp2
+    addImport("github_dot_com.dlclark.regexp2.Regexp2");
+    // install regexp2 lib
+    Sys.command("haxelib git go2hx_regexp2 https://github.com/go2hx/regexp2");
+    // create dox for regexp2
+    createDox("go2hx regexp2", "Precompiled regexp2 library in Haxe", "regexp2", "github_dot_com.dlclark.regexp2", "github_dot_com.dlclark.regexp2", "regexp2/api", "-lib go2hx_regexp2");
 }
 
-function addImports() {
+function createDox(title:String, desc:String, repo:String, topLevel:String, inStr:String, output:String, command:String) {
+    if (!sys.FileSystem.exists('page/$output'))
+        sys.FileSystem.createDirectory('page/output');
+    sys.io.File.copy("page/logoapi.svg", 'page/$output/logo.svg');
+    Sys.command('haxe dox.hxml $command');
+    final version = haxe.Json.parse(sys.io.File.getContent("go2hx/haxelib.json")).version;
+    Sys.command('haxelib run dox -D description "$desc" -D toplevel-package $topLevel -D website https://go2hx.github.io -D logo logo.svg -i go2hx.xml -o page/$output --title "$title" -D version $version -in $inStr -D source-path https://github.com/go2hx/$repo/blob/master/"');
+}
+
+function addStdImports() {
     final list:Array<String> = haxe.Json.parse(sys.io.File.getContent("go2hx/tests/std_hl.json"));
     final imps = [];
     for (std in list) {
@@ -22,6 +34,14 @@ function addImports() {
         imps.push('import $path;');
     }
     imps.push("function main() {}");
+    sys.io.File.saveContent("DoxMain.hx", imps.join("\n"));
+}
+
+function addImport(imp:String) {
+    final imps = [
+        'import "$imp";',
+        "function main() {}",
+    ];
     sys.io.File.saveContent("DoxMain.hx", imps.join("\n"));
 }
 
