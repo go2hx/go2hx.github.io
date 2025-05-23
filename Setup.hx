6a9115a076fa0d99a4de78a25bccccc61daa2d7f
Joey <9813078+PXshadow@users.noreply.github.com>
2025-05-23 18:39:21 +0300

    Setup: failed to run stdgo generator

--- Setup.hx
+++ Setup.hx
@@ -8,5 +8,6 @@ function main() {
     if (!FileSystem.exists("go2hx"))
         Sys.command("git clone --depth 1 https://github.com/go2hx/go2hx");
     Sys.setCwd("go2hx");
-    Sys.command("haxe scripts/extra/stdgo.hxml");
+    if (Sys.command("haxe extra/scripts/stdgo.hxml") != 0)
+        throw "failed to run stdgo generator";
 }
